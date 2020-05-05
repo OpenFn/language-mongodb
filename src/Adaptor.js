e325616a46cdbab2d97f148d20f7e18b20fa2477
Taylor Downs <downs.taylor@gmail.com>
2020-05-05 08:58:04 -0600

    insert documents

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -77,154 +77,44 @@ function connect(state) {
 function disconnect(state) {
   state.client.close();
   delete state.client;
-  // delete state.db;
   return state;
 }
 
-// /**
-//  * Make a GET request and POST the response somewhere else without failing.
-//  */
-// export function fetch2016(params) {
-//   var insertDocuments = function (db, jsonArray, callback) {
-//     // Get the documents collection
-//     var collection = db.collection('simce_2016_school_records');
-//     // Insert some documents
-//     collection.insertMany(jsonArray, function (err, result) {
-//       assert.equal(err, null);
-//       console.log(result.insertedCount);
-//       callback(result);
-//     });
-//   };
+/**
+ * Removes connection from the state.
+ * @example
+ *  disconnect(state)
+ * @function
+ * @param {object} params - Configuration for mongo
+ * @returns {State}
+ */
+export function insertDocuments(params) {
+  return (state) => {
+    const { client } = state;
 
-//   return (state) => {
-//     const { codes } = expandReferences(params)(state);
-//     const { endpoint, salt, mongoConnectionUrl } = state.configuration;
+    const { database, collection, documents, callback } = expandReferences(
+      params
+    )(state);
 
-//     const baseUrl = 'http://www.simce.cl';
+    const db = client.db(database);
+    const mCollection = db.collection(collection);
 
-//     var schools = [];
-
-//     codes.forEach(function (code) {
-//       // var code = element;
-//       var secretEndpoint =
-//         endpoint +
-//         'ficha-' +
-//         code +
-//         '_' +
-//         resumen(salt + base64.encode('ficha-' + code + '.json')) +
-//         '.json';
-//       var url = resolveUrl(baseUrl + '/', secretEndpoint);
-//       console.log('Performing a GET on URL: ' + url);
-
-//       try {
-//         var res = request('GET', url);
-//         const jsonBody = JSON.parse(res.getBody().toString('utf8'));
-//         console.log('Successfully fetched RBD ' + code);
-//         schools.push(jsonBody);
-//         // console.log(JSON.stringify(jsonBody, null, 2));
-//       } catch (e) {
-//         console.log('Failed to fetch RBD ' + code);
-//       }
-//     });
-
-//     // Connection URL
-//     var url = mongoConnectionUrl;
-
-//     // Use connect method to connect to the server
-//     MongoClient.connect(
-//       url,
-//       {
-//         // server: {
-//         // socketOptions: {
-//         // Setting a Very Long ten minute timeout...
-//         connectTimeoutMS: 1200000,
-//         socketTimeoutMS: 1200000,
-//         // }
-//         // }
-//       },
-//       function (err, db) {
-//         assert.equal(null, err);
-//         console.log('Connected successfully to server');
-
-//         insertDocuments(db, schools, function () {
-//           db.close();
-//         });
-//       }
-//     );
-//   };
-// }
-
-// /**
-//  * Make a GET request and POST the response somewhere else without failing.
-//  */
-// export function fetch2015(params) {
-//   var insertDocuments = function (db, jsonArray, callback) {
-//     // Get the documents collection
-//     var collection = db.collection('simce_2015_school_records');
-//     // Insert some documents
-//     collection.insertMany(jsonArray, function (err, result) {
-//       assert.equal(err, null);
-//       console.log(result.insertedCount);
-//       callback(result);
-//     });
-//   };
-
-//   return (state) => {
-//     const { codes } = expandReferences(params)(state);
-//     const { endpoint, salt, mongoConnectionUrl } = state.configuration;
-
-//     const baseUrl = 'http://www.simce.cl';
-
-//     var schools = [];
-
-//     codes.forEach(function (code) {
-//       // var code = element;
-//       var secretEndpoint =
-//         endpoint +
-//         'ficha_est-' +
-//         code +
-//         '_' +
-//         resumen(salt + base64.encode('ficha-' + code + '.xml')) +
-//         '.xml';
-//       var url = resolveUrl(baseUrl + '/', secretEndpoint);
-//       console.log('Performing a GET on URL: ' + url);
-
-//       try {
-//         var res = request('GET', url);
-//         const jsonBody = JSON.parse(parser.toJson(res.getBody()));
-//         console.log('Successfully fetched RBD ' + code);
-//         schools.push(jsonBody.establecimientos.estab);
-//         console.log(jsonBody);
-//       } catch (e) {
-//         console.log('Failed to fetch RBD ' + code);
-//       }
-//     });
-
-//     // Connection URL
-//     var url = mongoUrl;
-
-//     // Use connect method to connect to the server
-//     MongoClient.connect(
-//       url,
-//       {
-//         // server: {
-//         // socketOptions: {
-//         connectTimeoutMS: 360000,
-//         socketTimeoutMS: 360000,
-//         // }
-//         // }
-//       },
-//       function (err, db) {
-//         assert.equal(null, err);
-//         console.log('Connected successfully to server');
-
-//         insertDocuments(db, schools, function () {
-//           db.close();
-//         });
-//       }
-//     );
-//   };
-// }
+    return new Promise((resolve, reject) => {
+      mCollection.insertMany(documents, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(
+            `Inserted ${documents.length} documents into the collection`
+          );
+          const nextState = composeNextState(state, result);
+          if (callback) resolve(callback(nextState));
+          resolve(nextState);
+        }
+      });
+    });
+  };
+}
 
 export {
   field,
