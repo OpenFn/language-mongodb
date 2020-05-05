bd54af59d136b34dad2c3819e3a20acd7da4654c
Taylor Downs <downs.taylor@gmail.com>
2020-05-05 06:30:51 -0600

    initial clone

--- README.md
+++ README.md
@@ -97,3 +97,56 @@ update("counter", field(
 	field("number", dataValue("a")
 ))
 ```
+
+====================
+
+Language Simce [![Build Status](https://travis-ci.org/OpenFn/language-simce.svg?branch=master)](https://travis-ci.org/OpenFn/language-zurdo)
+==============
+
+Language Pack for extracting Chilean Schools Data.
+
+**Node 7.6**
+
+Documentation
+-------------
+
+#### sample configuration
+```json
+{
+  "endpoint": "data/",
+  "salt": "GedgafmgTicSimce2015",
+  "mongoConnectionUrl": "mongodb://<USERNAME>:<PASSWORD>!@full-connection-string/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+}
+```
+
+#### fetch2015
+```js
+fetch2015({
+  codes: [8873, 8874]
+});
+```
+
+#### fetch2016
+```js
+fetch2016({
+  codes: [
+    1,
+    2,
+    3,
+    4,
+    5
+  ]
+});
+```
+
+[Docs](docs/index)
+
+
+Development
+-----------
+
+Clone the repo, run `npm install`.
+
+Run tests using `npm run test` or `npm run test:watch`
+
+Build the project using `make`.
--- src/Adaptor.js
+++ src/Adaptor.js
@@ -1,95 +1,23 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.lastReferenceValue = exports.dataValue = exports.dataPath = exports.merge = exports.each = exports.alterState = exports.sourceValue = exports.fields = exports.field = undefined;
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-exports.execute = execute;
-//%*So this session needs to be abstracted. What exactly are fetch2016, fetch2015
-exports.fetch2016 = fetch2016;
-exports.fetch2015 = fetch2015;
-
-var _languageCommon = require('language-common');
-
-Object.defineProperty(exports, 'field', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.field;
-  }
-});
-Object.defineProperty(exports, 'fields', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.fields;
-  }
-});
-Object.defineProperty(exports, 'sourceValue', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.sourceValue;
-  }
-});
-Object.defineProperty(exports, 'alterState', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.alterState;
-  }
-});
-Object.defineProperty(exports, 'each', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.each;
-  }
-});
-Object.defineProperty(exports, 'merge', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.merge;
-  }
-});
-Object.defineProperty(exports, 'dataPath', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.dataPath;
-  }
-});
-Object.defineProperty(exports, 'dataValue', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.dataValue;
-  }
-});
-Object.defineProperty(exports, 'lastReferenceValue', {
-  enumerable: true,
-  get: function get() {
-    return _languageCommon.lastReferenceValue;
-  }
-});
-
-var _url = require('url');
-
-var _base = require('base-64');
-
-var _base2 = _interopRequireDefault(_base);
-
-var _utf = require('utf8');
-
-var _utf2 = _interopRequireDefault(_utf);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+import {
+  execute as commonExecute,
+  expandReferences
+} from 'language-common';
 var request = require('sync-request');
 // import request from 'request';
-
+import {
+  resolve as resolveUrl
+} from 'url';
+import base64 from 'base-64';
+import utf8 from 'utf8';
+import {
+  resumen
+} from './resumen.min.js';
 
 var parser = require('xml2json');
-var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
-var Buffer = require('buffer/').Buffer; // note: the trailing slash is important!
+var Buffer = require('buffer/').Buffer  // note: the trailing slash is important!
 
 /** @module Adaptor */
 
@@ -105,134 +33,125 @@ var Buffer = require('buffer/').Buffer; // note: the trailing slash is important
  * @param {Operations} operations - Operations to be performed.
  * @returns {Operation}
  */
-function execute() {
-  for (var _len = arguments.length, operations = Array(_len), _key = 0; _key < _len; _key++) {
-    operations[_key] = arguments[_key];
-  }
-
-  var initialState = {
+export function execute(...operations) {
+  const initialState = {
     references: [],
     data: null
+  }
+
+  return state => {
+    return commonExecute(...operations)({ ...initialState,
+      ...state
+    })
   };
 
-  return function (state) {
-    return _languageCommon.execute.apply(undefined, operations)(_extends({}, initialState, state));
-  };
 }
 
 /**
  * Make a GET request and POST the response somewhere else without failing.
  */
-function fetch2016(params) {
+export function fetch2016(params) {
 
-  var insertDocuments = function insertDocuments(db, jsonArray, callback) {
+  var insertDocuments = function(db, jsonArray, callback) {
     // Get the documents collection
     var collection = db.collection('simce_2016_school_records');
     // Insert some documents
-    collection.insertMany(jsonArray, function (err, result) {
+    collection.insertMany(
+      jsonArray
+    , function(err, result) {
       assert.equal(err, null);
       console.log(result.insertedCount);
       callback(result);
     });
-  };
+  }
 
-  return function (state) {
-    var _expandReferences = (0, _languageCommon.expandReferences)(params)(state),
-        codes = _expandReferences.codes;
+  return state => {
 
-    var _state$configuration = state.configuration,
-        endpoint = _state$configuration.endpoint,
-        salt = _state$configuration.salt,
-        mongoConnectionUrl = _state$configuration.mongoConnectionUrl;
+    const { codes } = expandReferences(params)(state);
+    const { endpoint, salt, mongoConnectionUrl } = state.configuration;
 
-
-    var baseUrl = "http://www.simce.cl";
-	//*% Remove this
+    const baseUrl = "http://www.simce.cl";
 
     var schools = [];
-	//*% Remove this
 
     codes.forEach(function (code) {
       // var code = element;
-      //*% Modify this 'ficha'- which is clearly specific to Chile. 
-	  var secretEndpoint = endpoint + "ficha-" + code + "_" + (0, _resumenMin.resumen)(salt + _base2.default.encode("ficha-" + code + ".json")) + ".json";
-      var url = (0, _url.resolve)(baseUrl + '/', secretEndpoint);
+      var secretEndpoint = (endpoint + "ficha-" + code + "_" + resumen(salt + base64.encode("ficha-" + code + ".json")) + ".json");
+      var url = resolveUrl(baseUrl + '/', secretEndpoint);
       console.log("Performing a GET on URL: " + url);
 
       try {
         var res = request('GET', url);
-        var jsonBody = JSON.parse(res.getBody().toString('utf8'));
+        const jsonBody = JSON.parse(res.getBody().toString('utf8'));
         console.log("Successfully fetched RBD " + code);
-        //*% Remove the mention of schools below. What should replace it? 
-		schools.push(jsonBody);
-        console.log(JSON.stringify(jsonBody, null, 2));
+        schools.push(jsonBody);
+        // console.log(JSON.stringify(jsonBody, null, 2));
       } catch (e) {
         console.log("Failed to fetch RBD " + code);
       }
     });
 
     // Connection URL
-    var url = mongoConnectionUrl;
+    var url = mongoConnectionUrl
 
     // Use connect method to connect to the server
     MongoClient.connect(url, {
       // server: {
-      // socketOptions: {
-      connectTimeoutMS: 360000,
-      socketTimeoutMS: 360000
+        // socketOptions: {
+          // Setting a Very Long ten minute timeout...
+          connectTimeoutMS:1200000,
+          socketTimeoutMS:1200000
+        // }
       // }
-      // }
-    }, function (err, db) {
+    }, function(err, db) {
       assert.equal(null, err);
       console.log("Connected successfully to server");
 
-      insertDocuments(db, schools, function () {
+      insertDocuments(db, schools, function() {
         db.close();
       });
     });
+
   };
 };
+
 
 /**
  * Make a GET request and POST the response somewhere else without failing.
  */
-//*% fetch2015 needs to be replaced. But by what?
- function fetch2015(params) {
+export function fetch2015(params) {
 
-  var insertDocuments = function insertDocuments(db, jsonArray, callback) {
+  var insertDocuments = function(db, jsonArray, callback) {
     // Get the documents collection
     var collection = db.collection('simce_2015_school_records');
     // Insert some documents
-    collection.insertMany(jsonArray, function (err, result) {
+    collection.insertMany(
+      jsonArray
+    , function(err, result) {
       assert.equal(err, null);
       console.log(result.insertedCount);
       callback(result);
     });
-  };
+  }
 
-  return function (state) {
-    var _expandReferences2 = (0, _languageCommon.expandReferences)(params)(state),
-        codes = _expandReferences2.codes;
+  return state => {
 
-    var _state$configuration2 = state.configuration,
-        endpoint = _state$configuration2.endpoint,
-        salt = _state$configuration2.salt,
-        mongoConnectionUrl = _state$configuration2.mongoConnectionUrl;
+    const { codes } = expandReferences(params)(state);
+    const { endpoint, salt, mongoConnectionUrl } = state.configuration;
 
-
-    var baseUrl = "http://www.simce.cl";
+    const baseUrl = "http://www.simce.cl";
 
     var schools = [];
 
     codes.forEach(function (code) {
       // var code = element;
-      var secretEndpoint = endpoint + "ficha_est-" + code + "_" + (0, _resumenMin.resumen)(salt + _base2.default.encode("ficha-" + code + ".xml")) + ".xml";
-      var url = (0, _url.resolve)(baseUrl + '/', secretEndpoint);
+      var secretEndpoint = (endpoint + "ficha_est-" + code + "_" + resumen(salt + base64.encode("ficha-" + code + ".xml")) + ".xml");
+      var url = resolveUrl(baseUrl + '/', secretEndpoint);
       console.log("Performing a GET on URL: " + url);
 
       try {
         var res = request('GET', url);
-        var jsonBody = JSON.parse(parser.toJson(res.getBody()));
+        const jsonBody = JSON.parse(parser.toJson(res.getBody()));
         console.log("Successfully fetched RBD " + code);
         schools.push(jsonBody.establecimientos.estab);
         console.log(jsonBody);
@@ -242,23 +161,37 @@ function fetch2016(params) {
     });
 
     // Connection URL
-    var url = mongoUrl;
+    var url = mongoUrl
 
     // Use connect method to connect to the server
     MongoClient.connect(url, {
       // server: {
-      // socketOptions: {
-      connectTimeoutMS: 360000,
-      socketTimeoutMS: 360000
+        // socketOptions: {
+          connectTimeoutMS:360000,
+          socketTimeoutMS:360000
+        // }
       // }
-      // }
-    }, function (err, db) {
+    }, function(err, db) {
       assert.equal(null, err);
       console.log("Connected successfully to server");
 
-      insertDocuments(db, schools, function () {
+      insertDocuments(db, schools, function() {
         db.close();
       });
     });
+
   };
 };
+
+export {
+  field,
+  fields,
+  sourceValue,
+  alterState,
+  each,
+  merge,
+  dataPath,
+  dataValue,
+  lastReferenceValue
+}
+from 'language-common';
